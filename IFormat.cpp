566798f93d750a480f91a223ffc11c2e17545a20
AliaMYH <aliahassan95@aucegypt.edu>
2015-03-25 15:31:35 +0200

    i again

--- IFormat.cpp
+++ IFormat.cpp
@@ -103,6 +103,157 @@ void IFormat::display ()
             break;
             
         case 0x28://SB
+            cout << "\tSB\t";
+            displayReg(rt);
+            cout<< ","<< dec << imm;
+            cout<< "(";
+            displayReg(rs);
+            cout <<")"<<endl;
+            
+            break;
+            
+        case 0x21://LH
+            cout << "\tLH\t";
+            displayReg(rt);
+            cout<< ","<< dec << imm;
+            cout<< "(";
+            displayReg(rs);
+            cout <<")"<<endl;
+            
+            break;
+            
+        case 0x29://SH
+            cout << "\tSH\t";
+            displayReg(rt);
+            cout<< ","<< dec << imm;
+            cout<< "(";
+            displayReg(rs);
+            cout <<")"<<endl;
+            
+            break;
+            
+        case 0x4:
+            cout << "\tBEQ\t";
+            displayReg(rt);
+            cout<< ",";
+            displayReg(rs);
+            cout<< ","<< dec << imm<<endl;
+            
+            break;
+            
+        case 0x5:
+            cout << "\tBNE\t";
+            displayReg(rt);
+            cout<< ",";
+            displayReg(rs);
+            cout<< ","<< dec << imm<<endl;
+            
+            break;
+            
+        case 0xa:
+            cout << "\tSLTI\t";
+            displayReg(rt);
+            cout<< ",";
+            displayReg(rs);
+            cout<< ","<< dec << imm<<endl;
+            
+            break;
+            
+        case 0xf:
+            cout << "\tLUI\t";
+            displayReg(rt);
+            cout<< ","<< dec << imm<<endl;
+            
+            break;
+
+
+            
+        default:
+            cout << "\tUnkown I-Format Instruction" << endl;
+            
+    }
+}
+bool IFormat::execute ()
+{
+    switch (opcode)
+    {
+        case 0x8:
+            cout << "\tADDI\t";
+            displayReg(rt);
+            cout<< ",";
+            displayReg(rs);
+            cout<< ","<< dec << imm<<endl;
+            
+            break;
+            
+        case 0x9:
+            cout << "\tADDIU\t";
+            displayReg(rt);
+            cout<< ",";
+            displayReg(rs);
+            cout<< ","<< dec << imm<<endl;
+            
+            break;
+            
+        case 0xc:
+            cout << "\tANDI\t";
+            displayReg(rt);
+            cout<< ",";
+            displayReg(rs);
+            cout<< ","<< dec << imm<<endl;
+            
+            break;
+            
+        case 0xd:
+            cout << "\tORI\t";
+            displayReg(rt);
+            cout<< ",";
+            displayReg(rs);
+            cout<< ","<< dec << imm<<endl;
+            
+            break;
+            
+        case 0xe:
+            cout << "\tXORI\t";
+            displayReg(rt);
+            cout<< ",";
+            displayReg(rs);
+            cout<< ","<< dec << imm<<endl;
+            
+            break;
+            
+        case 0x23://LW
+            
+            cout << "\tLW\t";
+            displayReg(rt);
+            cout<< ","<< dec << imm;
+            cout<< "(";
+            displayReg(rs);
+            cout <<")"<<endl;
+            
+            break;
+            
+        case 0x2b://SW
+            cout << "\tSW\t";
+            displayReg(rt);
+            cout<< ","<< dec << imm;
+            cout<< "(";
+            displayReg(rs);
+            cout <<")"<<endl;
+            
+            break;
+            
+        case 0x20://LB
+            cout << "\tLB\t";
+            displayReg(rt);
+            cout<< ","<< dec << imm;
+            cout<< "(";
+            displayReg(rs);
+            cout <<")"<<endl;
+            
+            break;
+            
+        case 0x28://SB
             cout << "\tLB\t";
             displayReg(rt);
             cout<< ","<< dec << imm;
@@ -131,25 +282,68 @@ void IFormat::display ()
             cout <<")"<<endl;
             
             break;
-
-
-
+            
+        case 0x4:
+            cout << "\tBEQ\t";
+            displayReg(rt);
+            cout<< ",";
+            displayReg(rs);
+            cout<< ","<< dec << imm<<endl;
+            
+            break;
+            
+        case 0x5:
+            cout << "\tBNE\t";
+            displayReg(rt);
+            cout<< ",";
+            displayReg(rs);
+            cout<< ","<< dec << imm<<endl;
+            
+            break;
+            
+        case 0xa:
+            cout << "\tSLTI\t";
+            displayReg(rt);
+            cout<< ",";
+            displayReg(rs);
+            cout<< ","<< dec << imm<<endl;
+            
+            break;
+            
+        case 0xf:
+            cout << "\tLUI\t";
+            displayReg(rt);
+            cout<< ","<< dec << imm<<endl;
+            
+            break;
+            
+            
             
         default:
             cout << "\tUnkown I-Format Instruction" << endl;
             
-    }
-}
-bool IFormat::execute ()
-{
-    switch (opcode)
-    {
         case 0x23:
+        {
             signedImm 		= (imm & 0x8000) ? (0xFFFF0000 | imm): imm;	// sign extending the immediate field
             address 	= registers[rs] + signedImm;
             break;
+        }
+            
+            
     }
+    
+    //for now
+    return true;
 }
+
+
+
+
+
+
+
+
+
 
 
 
