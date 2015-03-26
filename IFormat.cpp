42bcb5847af3703a78704b54c655168deec5641e
AliaMYH <aliahassan95@aucegypt.edu>
2015-03-26 12:04:33 +0200

    i format functions

--- IFormat.cpp
+++ IFormat.cpp
@@ -179,8 +179,22 @@ bool IFormat::execute ()
     {
         case 0x08://ADDI
            
-            //
-            //
+            if(((registers[rs]& 0x80000000)^(signedImm & 0x80000000))){
+                // Same sign
+                int temp=registers[rs] + signedImm;
+                
+                if(!((temp&0x80000000)^(registers[rs]&0x80000000)))
+                {
+                    // sign of result != sign of operands => overflow!
+                    cout <<"OVERFLOW"<<endl;
+                }
+                else
+                    registers[rt]=temp;
+            }
+            else{
+                // no overflow (signs are different)
+                registers[rt]=registers[rs] + signedImm;
+            }
             
             break;
             
@@ -207,29 +221,42 @@ bool IFormat::execute ()
             
         case 0x23://LW
             
-            //
-            //
+            for(int i=0; i<4;i++)
+            {
+                registers[rt]= registers[rt] | (memory[rs +signedImm - 0x10010000+i]>>(24-8*i)& 0x000000ff);
+            }
+            
             
             break;
             
         case 0x2b://SW
-            //
-            //
+            
+            for(int i=0; i<4;i++)
+            {
+                memory[rs +signedImm - 0x10010000 +i]= (registers[rt]>>(24-8*i)& 0x000000ff);
+                
+            }
+
             
             break;
             
         case 0x20://LB
-
+            registers[rt]= memory[rs +signedImm - 0x10010000];
             
             break;
             
         case 0x28://SB
-
+            memory[rs +signedImm - 0x10010000] = registers[rt];
             
             break;
             
         case 0x21://LH
             
+            for(int i=0; i<2;i++)
+            {
+                registers[rt]= registers[rt] | (memory[rs +signedImm - 0x10010000]>>(24-8*i)& 0x000000ff);
+            }
+
             break;
             
         case 0x29://SH
@@ -238,20 +265,28 @@ bool IFormat::execute ()
             
         case 0x04://BEQ
 
+            if(registers[rt]==registers[rs])
+                pc=pc+signedImm;
+           
             break;
             
         case 0x05: // BNE
-
+            if(registers[rt]!=registers[rs])
+                pc=pc+signedImm;
             
             break;
             
         case 0x0a: //SLTI
-
+            if(registers[rs]<signedImm)
+                registers[rt]=1;
+            else
+                registers[rt]=0;
             
             break;
             
         case 0x0f://LUI
 
+            registers[rt] = imm<<16 |0x0000;
             break;
             
        
